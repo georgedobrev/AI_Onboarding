f4e28045a39db73e4ab64c315188de7a758bb54e
borislavpanov-blankfactor <131446895+borislavpanov-blankfactor@users.noreply.github.com>
2023-05-22 14:06:06 +0300

    updates

---
+++ FrontEnd/src/components/navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { List, ListItem, ListItemIcon } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import MessageIcon from "@mui/icons-material/Message";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="navbar">
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <MessageIcon />
+          </ListItemIcon>
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <AccountCircleIcon />
+          </ListItemIcon>
+        </ListItem>
+      </List>
+    </div>
+  );
+};
+
+export default Navbar;
