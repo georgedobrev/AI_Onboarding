5b7a1c354dd3e8316ee6b23fc22b69ad76b95efb
Borislav Panov <borislav.panov@blankfactor.com>
2023-05-22 21:08:37 +0300

    h1

--- src/FrontEnd/src/App.tsx
+++ src/FrontEnd/src/App.tsx
@@ -3,12 +3,17 @@ import Navbar from "./components/navbar.tsx";
 
 function App() {
     return (
-        <>
-      <div className="App">
-          <Navbar/>
-          <div className="line"></div>
-      </div>
-    </>
+        <div className="App">
+            <Navbar />
+            <div className="line"></div>
+            <div className="heading-container">
+                <h1>Chat Bot - Blankfactor</h1>
+            </div>
+            <div className="cross-line"></div>
+            <div className="content">
+                {/* Content goes here */}
+            </div>
+        </div>
     );
 }
 
