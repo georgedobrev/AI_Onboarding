fa6159e7476365d649b7f95b5fe3e66ec7f2557a
Hristo Chipev <131447097+HristoChipev@users.noreply.github.com>
2023-05-23 13:15:06 +0300

    Data Layer Skeleton (#2)
    * Implemented migrations, DBContext  and UserConfigurator
    
    * Finished ModelBuilding and data layer skeleton
    
    * Fixed Docker connection
    
    * Removed unnecessary json setup and fixed migrations
    
    * Added .DS_Store to .gitignore
    
    ---------
    
    Co-authored-by: Hristo Chipev <107315850+HChipev@users.noreply.github.com>

---
+++ src/Back end/AI_Onboarding.Data/ModelBuilding/UserClaimsConfigurator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AI_Onboarding.Data.ModelBuilding
+{
+    public class UserClaimsConfigurator : IEntityTypeConfiguration<IdentityUserClaim<string>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserClaim<string>> builder)
+        {
+            builder.ToTable("UserClaims");
+        }
+    }
+}
