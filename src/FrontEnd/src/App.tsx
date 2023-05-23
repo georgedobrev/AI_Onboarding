620145461dd08317cbab8dc382fe4aa3ecf46048
Borislav Panov <borislav.panov@blankfactor.com>
2023-05-23 21:01:48 +0300

    update

--- src/FrontEnd/src/App.tsx
+++ src/FrontEnd/src/App.tsx
@@ -1,26 +1,24 @@
-import "./assets/App.css";
-import Navbar from "./components/navbar.tsx";
+import './assets/App.css';
+import Navbar from './components/navbar.tsx';
 
 function App() {
-    return (
+  return (
     <div className="App">
-
-      <Navbar/>
+      <Navbar />
 
       <div className="content-container">
         <div className="heading-container">
           <h1 className="app-title">Chat Bot - Blankfactor</h1>
           <div className="header-line"></div>
         </div>
-          <div className="chat-box">
         <div className="chat-container">
-          <div className="chat-line"></div>
           <h2>Chats</h2>
+          <div className="vertical-chatline"></div>
+          <div className="horizontal-chatline"></div>
         </div>
-          </div>
       </div>
     </div>
-    );
+  );
 }
 
 export default App;
