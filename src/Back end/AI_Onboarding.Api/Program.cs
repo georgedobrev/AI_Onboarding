351a7ddd96989593c3ac1c32c1ea9326e9fe5c20
aleksandararsov <aleksandar.arsov@blankfactor.com>
2023-05-23 10:19:08 +0300

    ServiceExstensions

--- src/Back end/AI_Onboarding.Api/Program.cs
+++ src/Back end/AI_Onboarding.Api/Program.cs
@@ -14,6 +14,7 @@ namespace AI_Onboarding.Api
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
