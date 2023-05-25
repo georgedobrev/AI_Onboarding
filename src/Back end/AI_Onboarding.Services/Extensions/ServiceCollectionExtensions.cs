a2081bbe4b6a6dc389b2c250409ff9e29501a503
aleksandararsov <aleksandar.arsov@blankfactor.com>
2023-05-25 15:51:29 +0300

    Fixed

---
+++ src/Back end/AI_Onboarding.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace AI_Onboarding.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        
+        public static IServiceCollection RegisterDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("YourConnectionString");
+
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
--- src/IServiceCollectionExtensions.cs
+++
@@ -1,9 +0,0 @@
-
-public static class IServiceCollectionExtensions
-{
-
-    public static void AddServices(this.IServiceCollection services)
-    {
-        services.AddScoped<IFileService, FileService>();
-    }
-}
