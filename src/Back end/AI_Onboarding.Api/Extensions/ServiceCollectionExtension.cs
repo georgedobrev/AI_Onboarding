d1ad498542af2a8f5b6c1d3c5d871c96f68a9fad
aleksandararsov <131447151+aleksandararsov@users.noreply.github.com>
2023-05-26 17:15:29 +0300

    Ai onboarding.service (#3)
    * Service_Layer
    
    * Services
    
    * ServiceExstensions
    
    * Fixed
    
    * Services

---
+++ src/Back end/AI_Onboarding.Api/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace AI_Onboarding.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using AI_Onboarding.Data;
+
+public static class ServiceCollectionExtension
+{
+    
+    public static IServiceCollection RegisterDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        
+        services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("<from appsettings.{Environment}.json>")));
+    }
+}
+
