c06d58d3c75bb6cbc0510cb9d076a3ac3dd921b5
borislavpanov-blankfactor <131446895+borislavpanov-blankfactor@users.noreply.github.com>
2023-05-29 10:58:34 +0300

    added google signup

--- src/FrontEnd/src/components/SignIn/GoogleSignup.tsx
+++ src/FrontEnd/src/components/SignIn/GoogleSignup.tsx
@@ -1,10 +1,12 @@
-import { GoogleLogin } from "@react-oauth/google";
+import {GoogleLogin} from "@react-oauth/google";
 import './GoogleSignup.css';
-import React, { useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import React, {useEffect, useState} from "react";
+import {useNavigate, useLocation} from 'react-router-dom';
+import logoImage from '../../assets/blankfactor-logo.jpg';
 
 const GoogleSignup: React.FC = () => {
     const navigate = useNavigate();
+    const location = useLocation();
     const [isSignupSuccess, setSignupSuccess] = useState(false);
 
     useEffect(() => {
@@ -28,15 +30,31 @@ const GoogleSignup: React.FC = () => {
         // Handle login failure if needed
     };
 
+    const shouldRenderContainer = location.pathname !== '/home';
+
     return (
-        <div className="google-signup-container">
-            {!isSignupSuccess && (
-                <GoogleLogin
-                    onSuccess={handleGoogleSignupSuccess}
-                    onError={handleGoogleSignupError}
-                />
-            )}
-        </div>
+        shouldRenderContainer && (
+            <div className="google-signup-container">
+                <div className="google-signup-overlay">
+                    <div className="google-signup-header">
+                        <img src={logoImage} alt="blankfactor" className="google-signup-logo"/>
+                        <h1 className="google-signup-welcome-h1">Welcome to <br/> </h1>
+                        <span className="google-signup-welcome-span">Blankfactor ChatBot!</span>
+                    </div>
+                    <div className="google-signup-body">
+                        {/*<h1 className="google-signup-h1">Sign up with Google</h1>*/}
+                        {!isSignupSuccess && (
+                            <GoogleLogin
+                                onSuccess={handleGoogleSignupSuccess}
+                                onError={handleGoogleSignupError}
+                                text="signup_with"
+                                size="large"
+                            />
+                        )}
+                    </div>
+                </div>
+            </div>
+        )
     );
 }
 
