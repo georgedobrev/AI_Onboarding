d6b29c30e619d53a161ab4ccaed8528537bcc721
borislavpanov-blankfactor <131446895+borislavpanov-blankfactor@users.noreply.github.com>
2023-05-30 14:25:40 +0300

    updates login form

--- src/FrontEnd/src/components/SignIn/GoogleSignup.tsx
+++ src/FrontEnd/src/components/SignIn/GoogleSignup.tsx
@@ -1,8 +1,11 @@
-import {GoogleLogin} from "@react-oauth/google";
-import './GoogleSignup.css';
 import React, {useEffect, useState} from "react";
 import {useNavigate, useLocation} from 'react-router-dom';
+import {GoogleLogin} from "@react-oauth/google";
+import './GoogleSignup.css';
+import {TextField} from "@mui/material";
+import PasswordField from './PasswordField';
 import logoImage from '../../assets/blankfactor-logo.jpg';
+import Button from '@mui/material/Button';
 
 const GoogleSignup: React.FC = () => {
     const navigate = useNavigate();
@@ -18,8 +21,6 @@ const GoogleSignup: React.FC = () => {
 
     const handleGoogleSignupSuccess = (credentialResponse: any) => {
         console.log(credentialResponse);
-        // Perform any additional logic for successful sign-up
-        // Redirect to "/home" route
         navigate('/home');
         setSignupSuccess(true);
         localStorage.setItem('signupSuccess', 'true');
@@ -27,35 +28,40 @@ const GoogleSignup: React.FC = () => {
 
     const handleGoogleSignupError = () => {
         console.log('Login Failed');
-        // Handle login failure if needed
     };
 
     const shouldRenderContainer = location.pathname !== '/home';
 
     return (
         shouldRenderContainer && (
-            <div className="google-signup-container">
-                <div className="google-signup-overlay">
-                    <div className="google-signup-header">
+            <div className="signup-container">
+                <div className="signup-overlay">
+                    <div className="signup-header">
                         <img src={logoImage} alt="blankfactor" className="google-signup-logo"/>
-                        <h1 className="google-signup-welcome-h1">Welcome to <br/> </h1>
-                        <span className="google-signup-welcome-span">Blankfactor ChatBot!</span>
+                        <h1 className="signup-welcome-h1">Welcome to <br/></h1>
+                        <span className="signup-welcome-span">Blankfactor ChatBot!</span>
+                        <TextField id="outlined-basic" label="Email address" variant="outlined"
+                                   className="email-field"/>
+                        <PasswordField/>
+                        <Button variant="contained" className="signup-continue-btn">Continue</Button>
+                        <span className="signup-or">OR</span>
                     </div>
-                    <div className="google-signup-body">
-                        {/*<h1 className="google-signup-h1">Sign up with Google</h1>*/}
-                        {!isSignupSuccess && (
-                            <GoogleLogin
-                                onSuccess={handleGoogleSignupSuccess}
-                                onError={handleGoogleSignupError}
-                                text="signup_with"
-                                size="large"
-                            />
-                        )}
+                    <div className="signup-body">
+                        <div className="signup-button-container">
+                            {!isSignupSuccess && (
+                                <GoogleLogin
+                                    onSuccess={handleGoogleSignupSuccess}
+                                    onError={handleGoogleSignupError}
+                                    text="signup_with"
+                                    size="large"
+                                />
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
         )
     );
-}
+};
 
 export default GoogleSignup;
---
+++ src/FrontEnd/src/components/SignIn/PasswordField.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { TextField, IconButton, InputAdornment } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+
+const PasswordField = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <TextField
+            id="outlined-password-input"
+            label="Password"
+            type={showPassword ? 'text' : 'password'}
+            autoComplete="current-password"
+            className="password-field"
+            InputProps={{
+                endAdornment: (
+                    <InputAdornment position="end">
+                        <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                        >
+                            {showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                    </InputAdornment>
+                ),
+            }}
+        />
+    );
+};
+
+export default PasswordField;
