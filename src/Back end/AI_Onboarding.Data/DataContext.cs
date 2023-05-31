66bfc72a7065fb7a86ce2a505c06af9712c7aa39
HristoChipev <hristo.chipev@blankfactor.com>
2023-05-31 10:33:52 +0300

    Override SaveChanges

--- src/Back end/AI_Onboarding.Data/DataContext.cs
+++ src/Back end/AI_Onboarding.Data/DataContext.cs
@@ -7,8 +7,10 @@ namespace AI_Onboarding.Data
 {
     public class DataContext : IdentityDbContext<User, Role, int, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        private readonly User _currentUser;
+        public DataContext(DbContextOptions<DataContext> options, User currentUser) : base(options)
         {
+            _currentUser = currentUser;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -21,6 +23,32 @@ namespace AI_Onboarding.Data
             new UserTokensConfigurator().Configure(modelBuilder.Entity<UserToken>());
             new RoleConfigurator().Configure(modelBuilder.Entity<Role>());
             new RoleClaimsConfigurator().Configure(modelBuilder.Entity<RoleClaim>());
+        }
+
+        public override int SaveChanges()
+        {
+            var entities = ChangeTracker.Entries()
+                .Where(x => x.Entity is BaseEntity && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+            var currentDate = DateTime.UtcNow;
+
+            foreach (var entityEntry in entities)
+            {
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((BaseEntity)entityEntry.Entity).CreatedAt = currentDate;
+                }
+
+                ((BaseEntity)entityEntry.Entity).ModifiedAt = currentDate;
+
+                if (_currentUser is not null)
+                {
+                    ((BaseEntity)entityEntry.Entity).ModifiedBy = _currentUser;
+                    ((BaseEntity)entityEntry.Entity).ModifiedById = _currentUser.Id;
+                }
+            }
+
+            return base.SaveChanges();
         }
     }
 }
