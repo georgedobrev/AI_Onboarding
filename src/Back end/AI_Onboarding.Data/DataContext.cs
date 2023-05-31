dfff6f4f56b990da03375511136ef3fba7f586c1
Hristo Chipev <131447097+HristoChipev@users.noreply.github.com>
2023-05-31 18:06:13 +0300

    Feature AIO11 JWT Swagger config (#11)
    * Setup swagger
    
    * Override SaveChanges
    
    * Fixed override SaveChanges
    
    * Merge branch 'main' into Feature-AIO11-JWTSwaggerConfig
    
    ---------
    
    Co-authored-by: Hristo Chipev <107315850+HChipev@users.noreply.github.com>

--- src/Back end/AI_Onboarding.Data/DataContext.cs
+++ src/Back end/AI_Onboarding.Data/DataContext.cs
@@ -1,14 +1,22 @@
 ﻿using AI_Onboarding.Data.ModelBuilding;
 using AI_Onboarding.Data.Models;
+using AI_Onboarding.Data.Repository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace AI_Onboarding.Data
 {
     public class DataContext : IdentityDbContext<User, Role, int, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly Repository<User> _repository;
+
+        public DataContext(DbContextOptions<DataContext> options, IHttpContextAccessor httpContextAccessor, Repository<User> repository) : base(options)
         {
+            _httpContextAccessor = httpContextAccessor;
+            _repository = repository;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -21,6 +29,35 @@ namespace AI_Onboarding.Data
             new UserTokensConfigurator().Configure(modelBuilder.Entity<UserToken>());
             new RoleConfigurator().Configure(modelBuilder.Entity<Role>());
             new RoleClaimsConfigurator().Configure(modelBuilder.Entity<RoleClaim>());
+        }
+
+        public override int SaveChanges()
+        {
+            var entities = ChangeTracker.Entries()
+                .Where(x => x.Entity is BaseEntity && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+            var currentDate = DateTime.UtcNow;
+
+            foreach (var entityEntry in entities)
+            {
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((BaseEntity)entityEntry.Entity).CreatedAt = currentDate;
+                }
+
+                ((BaseEntity)entityEntry.Entity).ModifiedAt = currentDate;
+
+                _ = int.TryParse(_httpContextAccessor.HttpContext?.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value, out int userId);
+                var user = _repository.Find(userId);
+
+                if (user is not null)
+                {
+                    ((BaseEntity)entityEntry.Entity).ModifiedBy = user;
+                    ((BaseEntity)entityEntry.Entity).ModifiedById = userId;
+                }
+            }
+
+            return base.SaveChanges();
         }
     }
 }
