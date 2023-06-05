2c6131952512305b35062b94e2ddbffe5c5d2577
aleksandararsov <aleksandar.arsov@blankfactor.com>
2023-06-05 21:30:42 +0300

    Updated

--- src/Back end/AI_Onboarding.Api/Controllers/IdentityController.cs
+++ src/Back end/AI_Onboarding.Api/Controllers/IdentityController.cs
@@ -5,6 +5,8 @@ using AI_Onboarding.Services.Interfaces;
 using AI_Onboarding.ViewModels.JWTModels;
 using Microsoft.AspNetCore.Identity;
 using AI_Onboarding.ViewModels.UserModels.Profiles;
+using AI_Onboarding.Data.Models;
+using System.Security.Claims;
 
 namespace AI_Onboarding.Api.Controllers
 {
@@ -14,13 +16,15 @@ namespace AI_Onboarding.Api.Controllers
     {
         private readonly IIdentityService _identityServise;
         private readonly IConfiguration _configuration;
-        private readonly SignInManager<> _signInManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
 
-        public IdentityController(IIdentityService identityServise, IConfiguration configuration, SignInManager<> signInManager)
+        public IdentityController(IIdentityService identityServise, IConfiguration configuration, SignInManager<User> signInManager, UserManager<User> userManager)
         {
             _identityServise = identityServise;
             _configuration = configuration;
             _signInManager = signInManager;
+            _userManager = userManager;
 
         }
 
@@ -102,9 +106,70 @@ namespace AI_Onboarding.Api.Controllers
 
         [HttpPost]
         [AllowAnonymous]
-        public Task<ActionResult> GoogleLogin([FromBody] GoogleLogInModel googleLogIn)
+        public IActionResult ExternalLogin(string provider, string returnUrl)
         {
+            var redirectUrl = Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl });
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+            return new ChallengeResult(provider, properties);
+        }
 
+        
+        [AllowAnonymous]
+        public async Task<IActionResult> ExternalLoginCallback(string returnUrl = null, string remoteError = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            GoogleLogInModel googleLogInModel = new GoogleLogInModel
+            {
+                ReturnUrl = returnUrl,
+                ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList(),
+            };
+
+            if (remoteError != null)
+            {
+                ModelState.AddModelError(string.Empty, $"Errpr from external provider: {remoteError}");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+            if (info == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest , new {Message = "External LogIn info not found!"});
+            }
+
+            var user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+            if (user == null)
+            {
+                user = new User { UserName = info.Principal.FindFirstValue(ClaimTypes.Email), Email = info.Principal.FindFirstValue(ClaimTypes.Email) };
+                var result = await _userManager.CreateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    result = await _userManager.AddLoginAsync(user, info);
+                    if (result.Succeeded)
+                    {
+                        var randomPasswordHash = _userManager.PasswordHasher.HashPassword(user, Guid.NewGuid().ToString());
+                        user.PasswordHash = randomPasswordHash;
+                        await _userManager.UpdateAsync(user);
+                    }
+                }
+
+                if (!result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, new {Message = "User registration failed." });
+                }
+            }
+
+            await _signInManager.SignInAsync(user, isPersistent: false);
+
+            if (!string.IsNullOrEmpty(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return Ok();
+            }
         }
 
        
--- src/Back end/AI_Onboarding.ViewModels/UserModels/Profiles/GoogleLogInModel.cs
+++ src/Back end/AI_Onboarding.ViewModels/UserModels/Profiles/GoogleLogInModel.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
 namespace AI_Onboarding.ViewModels.UserModels.Profiles
 {
 	public class GoogleLogInModel
 	{
-		
-	}
+		public string Email { get; set; }
+
+		public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+    }
 }
 
