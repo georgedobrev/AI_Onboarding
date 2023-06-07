c8db0e8a17bb5efc3aed0699f03036fe64448709
Borislav Panov <borislav.panov@blankfactor.com>
2023-06-08 00:23:33 +0300

    added message bar

--- src/FrontEnd/src/components/Messages/Messages.tsx
+++ src/FrontEnd/src/components/Messages/Messages.tsx
@@ -1,19 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import SendIcon from '@mui/icons-material/Send';
+import IconButton from '@mui/material/IconButton';
 import './Messages.css';
 
 const Messages: React.FC = () => {
-  return (
-    <div className="messages-container">
-      <div className="messages-header">
-        <h2 className="messages-title">Messages</h2>
-        <div className="horizontal-messagesline"></div>
-      </div>
-      <div className="messages-content-container">
-        <div className="messages-content"></div>
-      </div>
-      <div className="vertical-messagesline"></div>;
-    </div>
-  );
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const handleSearchSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        // Perform search logic here based on the searchQuery
+        // You can interact with your chat bot or perform any other action
+    };
+
+    return (
+        <div className="messages-container">
+            <div className="messages-header">
+                <h2 className="messages-title">Messages</h2>
+                <div className="horizontal-messagesline"></div>
+            </div>
+            <div className="messages-content-container">
+                <div className="messages-content">
+                    <form onSubmit={handleSearchSubmit} className="search-container">
+                        <div className="search-input">
+                            <IconButton style={{ color: 'white' }}>
+                                <AttachFileIcon style={{ marginRight: '10px' }} />
+                            </IconButton>
+                            <div className="separator"></div>
+                            <TextField
+                                placeholder="Write a message"
+                                value={searchQuery}
+                                onChange={handleSearchChange}
+                                fullWidth
+                                InputProps={{
+                                    className: 'search-textfield',
+                                    style: {
+                                        color: 'white',
+                                    },
+                                }}
+                            />
+                            <IconButton style={{ color: 'white' }}>
+                                <SendIcon style={{ marginLeft: '10px' }} />
+                            </IconButton>
+                        </div>
+                    </form>
+                </div>
+                <div className="vertical-messagesline"></div>
+                <div className="files-content"></div>
+            </div>
+        </div>
+    );
 };
 
 export default Messages;
