457a283e6fc29b03d656c9de11ad1cb5c4bc156c
Borislav Panov <borislav.panov@blankfactor.com>
2023-06-09 22:46:33 +0300

    added logo and favicon

--- src/FrontEnd/src/components/Home/Home.tsx
+++ src/FrontEnd/src/components/Home/Home.tsx
@@ -3,6 +3,7 @@ import Navbar from '../Navbar/Navbar.tsx';
 import Chats from '../Chats/Chats.tsx';
 import Messages from '../Messages/Messages.tsx';
 import './Home.css';
+import blankfactorLogo from '../../assets/blankfactor-logo.jpg';
 
 const Home: React.FC = () => {
   return (
@@ -10,7 +11,10 @@ const Home: React.FC = () => {
       <Navbar />
       <div className="content-container">
         <div className="header-container">
-          <h1 className="header-title">Chat Bot - Blankfactor</h1>
+          <div className="header-logo-title">
+            <img src={blankfactorLogo} alt="Blankfactor Logo" className="header-logo" />
+            <h1 className="header-title">Chat Bot - Blankfactor</h1>
+          </div>
           <div className="header-line"></div>
         </div>
         <div className="main-container">
