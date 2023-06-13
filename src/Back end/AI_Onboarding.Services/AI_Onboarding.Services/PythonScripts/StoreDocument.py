3f6c678d3b8aa7f5d2da8f2f8c45f59bbb4e4da3
HristoChipev <hristo.chipev@blankfactor.com>
2023-06-13 15:08:15 +0300

    Change vector storing method

--- src/Back end/AI_Onboarding.Services/AI_Onboarding.Services/PythonScripts/StoreDocument.py
+++ src/Back end/AI_Onboarding.Services/AI_Onboarding.Services/PythonScripts/StoreDocument.py
@@ -1,11 +1,8 @@
 import sys
-from dotenv import load_dotenv
 import pinecone
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-# Load environment variables from .env file
-load_dotenv(".env")
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 
 # Load the FLAN-T5 model and tokenizer
 model_name = 'google/flan-t5-base'
@@ -27,25 +24,21 @@ document_text = sys.argv[1]
 
 chunks = text_splitter.split_text(text=document_text)
 
+# Initialize the HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings(model_name = model_name,model_kwargs = {'device': 'cpu'})
+
 # Encode text chunks to obtain vector representations
 chunk_vectors = []
 
 for chunk in chunks:
-    inputs = tokenizer(chunk, return_tensors="pt")
-    outputs = model.generate(**inputs, max_length=512, do_sample=False)
-    vector = outputs[0]
+    vector = embeddings.embed_query(chunk)
     chunk_vectors.append(vector)
-
-# Convert chunk_vectors to a list of lists with float values
-chunk_vectors = [vector.tolist() for vector in chunk_vectors]
 
 # Prepare data for indexing in Pinecone
 pinecone_data = []
 for idx, chunk in enumerate(chunks):
     vector = chunk_vectors[idx]
-    padded_vector = vector + [0.0] * (768 - len(vector))  # Pad the vector to match dimension 768
-    padded_vector = [float(value) for value in padded_vector]  # Convert values to float
-    pinecone_data.append({"id": str(idx), "values": padded_vector})
+    pinecone_data.append({"id": str(idx), "values": vector})
 
 # Index chunks in Pinecone
 pinecone.Index(pinecone_index).upsert(pinecone_data)
