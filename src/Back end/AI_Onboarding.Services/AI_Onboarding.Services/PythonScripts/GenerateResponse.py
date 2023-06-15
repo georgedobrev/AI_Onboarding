98ecee762000418e1f9e7253f55c1c0f6e9ac4e1
aleksandararsov <aleksandar.arsov@blankfactor.com>
2023-06-15 07:28:44 +0300

    Finished

--- src/Back end/AI_Onboarding.Services/AI_Onboarding.Services/PythonScripts/GenerateResponse.py
+++ src/Back end/AI_Onboarding.Services/AI_Onboarding.Services/PythonScripts/GenerateResponse.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pinecone
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 # Get the user's home directory
@@ -16,36 +17,35 @@ model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
 
 tokenizer = AutoTokenizer.from_pretrained(base_model)
 
-# Initialize Pinecone
+# Load the FLAN-T5 model and tokenizer
+model_name = "sentence-transformers/all-mpnet-base-v2"
+
+# Set up Pinecone client
 pinecone.init(api_key="ebe39065-b027-4b75-940b-aad3809f72e6", environment="us-west4-gcp")
+pinecone_index_name = "ai-onboarding"
+pinecone_index = pinecone.Index(index_name=pinecone_index_name)
 
-index_name = "ai-onboarding"
-
-index = pinecone.Index(index_name)
-
-
-def get_similar_docs(query, k=2, score=False):
-    if score:
-        similar_docs = index.similarity_search_with_score(query, k=k)
-    else:
-        similar_docs = index.similarity_search(query, k=k)["ids"]
-    return similar_docs
-
-
-def get_answer(query):
-    similar_docs = get_similar_docs(query)
-    context = similar_docs[0]["vector"]  # Use the vector of the most similar document as the context
-    inputs = tokenizer.encode_plus(query, context, return_tensors="pt")
-    outputs = model.generate(**inputs, no_repeat_ngram_size=2, min_length=30, max_new_tokens=500)
-    res = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return res
-
-
-# Get the question from command line arguments
 question = sys.argv[1]
 
-# Get the answer
-answer = get_answer(question)
+# Initialize the HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs={'device': 'cpu'})
 
-# Print the answer
-print(answer)
+vector = embeddings.embed_query(question)
+
+# Retrieve top 3 relevant vectors from Pinecone index
+pinecone_results = pinecone_index.query(vector, top_k=3,include_metadata=True)
+
+context = ""
+for match in pinecone_results['matches']:
+    metadata = match['metadata']
+    text = metadata['text']
+    context += text
+
+# Generate response based on the question and context
+inputs = tokenizer(f"question: {question} context: {context}", return_tensors="pt")
+
+outputs = model.generate(**inputs, max_new_tokens=5000)
+
+res = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+result_string = ' '.join(res)
+print(result_string)
