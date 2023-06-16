002ec483f1c2506a707265d453fb0035af0af5a6
Hristo Chipev <131447097+HristoChipev@users.noreply.github.com>
2023-06-16 09:24:29 +0300

    Feature AIO26 roles (#25)
    * Add seeding
    
    * Add Roles to JWT
    
    * Add Roles to array
    
    * Fix conflicts
    
    * Fix isLogin

---
+++ src/Back end/AI_Onboarding.Common/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace AI_Onboarding.Common
+{
+    public static class Roles
+    {
+        public const string Administrator = "Administrator";
+        public const string Employee = "Employee";
+    }
+}
+
