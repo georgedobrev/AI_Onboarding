45d879b214a2aa18e3dc746fb63899192951d7f9
borislavpanov-blankfactor <131446895+borislavpanov-blankfactor@users.noreply.github.com>
2023-06-20 11:12:03 +0300

    added watcher for localStorage in navbar

--- src/FrontEnd/src/components/Messages/FileUploader.tsx
+++ src/FrontEnd/src/components/Messages/FileUploader.tsx
@@ -1,40 +1,57 @@
 import React, { useState } from 'react';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import { Document, Page } from 'react-pdf';
+import DocViewer, { DocViewerRenderers } from '@cyntler/react-doc-viewer';
 import { apiService } from '../../services/apiService.ts';
 import config from '../../config.json';
 import './uploaded-file.css';
 import { Button, CircularProgress } from '@mui/material';
 
 const FileUploader: React.FC = () => {
-  const [pdfFile, setPdfFile] = useState<File | null>(null);
+  const [documentFiles, setDocumentFiles] = useState<File[]>([]);
+  const [documentType, setDocumentType] = useState<string | null>(null);
   const [numPages, setNumPages] = useState<number>(0);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [loading, setLoading] = useState<boolean>(false); // Added loading state
+  const [loading, setLoading] = useState<boolean>(false);
+  const [responseReceived, setResponseReceived] = useState<boolean>(false);
 
   const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = (event.target as HTMLInputElement)?.files?.[0] || null;
+    const files = (event.target as HTMLInputElement)?.files || null;
 
-    if (file) {
-      setPdfFile(file);
+    if (files) {
+      setDocumentFiles(Array.from(files));
       setCurrentPage(1);
-      setLoading(true); // Show loading spinner when file is selected
+      setLoading(true);
+      setResponseReceived(false);
     }
 
     try {
       const baseUrl = config.baseUrl;
       const uploadEndpoint = config.uploadEndpoint;
-      const response = await apiService.uploadFile(baseUrl, uploadEndpoint, file);
-      if (response) {
-        // File uploaded successfully
+
+      const responses = await Promise.all(
+        Array.from(files).map((file) => apiService.uploadFile(baseUrl, uploadEndpoint, file))
+      );
+
+      if (responses.every((response) => response)) {
+        const extension = files[0].name.split('.').pop()?.toLowerCase();
+        if (extension === 'pdf') {
+          setDocumentType('pdf');
+        } else if (extension === 'docx' || extension === 'pptx') {
+          setDocumentType(extension);
+        } else {
+          setDocumentType(null);
+          console.error('Unsupported file type:', extension);
+        }
+
+        setResponseReceived(true);
       } else {
-        // Error uploading file
-        console.error('Error uploading file:', response);
+        console.error('Error uploading files:', responses);
       }
     } catch (error) {
-      console.error('Error uploading file:', error);
+      console.error('Error uploading files:', error);
     } finally {
-      setLoading(false); // Hide loading spinner after API response
+      setLoading(false);
     }
   };
 
@@ -43,58 +60,79 @@ const FileUploader: React.FC = () => {
   };
 
   const handlePreviousPage = () => {
-    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+    if (responseReceived) {
+      setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+    }
   };
 
   const handleNextPage = () => {
-    setCurrentPage((prevPage) => Math.min(prevPage + 1, numPages));
+    if (responseReceived) {
+      setCurrentPage((prevPage) => Math.min(prevPage + 1, numPages));
+    }
   };
 
   return (
     <div className="pdf-main">
-      {pdfFile ? (
+      {documentFiles.length > 0 ? (
         <div className="pdf-container">
-          {loading ? ( // Display loading spinner while waiting for API response
+          {loading ? (
             <div className="loading-spinner">
               <CircularProgress size={24} />
             </div>
           ) : (
             <div className="pdf-wrapper">
-              <Document
-                file={pdfFile}
-                onLoadSuccess={handleDocumentLoadSuccess}
-                onLoadError={(error) => console.error('Error loading PDF:', error)}
-              >
-                <Page
-                  pageNumber={currentPage}
-                  className="pdf-page"
-                  renderMode="canvas"
-                  renderTextLayer={false}
-                  renderAnnotationLayer={false}
-                  renderInteractiveForms={false}
+              {documentType === 'pdf' && (
+                <Document
+                  file={documentFiles[0]}
+                  onLoadSuccess={handleDocumentLoadSuccess}
+                  onLoadError={(error) => console.error('Error loading PDF:', error)}
+                >
+                  <Page
+                    pageNumber={currentPage}
+                    className="pdf-page"
+                    renderMode="canvas"
+                    renderTextLayer={false}
+                    renderAnnotationLayer={false}
+                  />
+                </Document>
+              )}
+              {documentType === 'docx' && (
+                <DocViewer
+                  renderer={DocViewerRenderers.Docx}
+                  documents={documentFiles.map((file) => URL.createObjectURL(file))}
+                  onError={(error) => console.error('Error loading DOCX:', error)}
                 />
-              </Document>
+              )}
+              {documentType === 'pptx' && (
+                <DocViewer
+                  renderer={DocViewerRenderers.Pptx}
+                  documents={documentFiles.map((file) => URL.createObjectURL(file))}
+                  onError={(error) => console.error('Error loading PPTX:', error)}
+                />
+              )}
             </div>
           )}
-          <div className="pdf-navigation">
-            <Button
-              onClick={handlePreviousPage}
-              disabled={currentPage === 1}
-              className="pdf-previous-page"
-              variant="contained"
-            >
-              Previous Page
-            </Button>
-            <span>{`${currentPage} / ${numPages}`}</span>
-            <Button
-              onClick={handleNextPage}
-              disabled={currentPage === numPages}
-              className="pdf-next-page"
-              variant="contained"
-            >
-              Next Page
-            </Button>
-          </div>
+          {responseReceived && (
+            <div className="pdf-navigation">
+              <Button
+                onClick={handlePreviousPage}
+                disabled={currentPage === 1}
+                className="pdf-previous-page"
+                variant="contained"
+              >
+                Previous Page
+              </Button>
+              <span>{`${currentPage} / ${numPages}`}</span>
+              <Button
+                onClick={handleNextPage}
+                disabled={currentPage === numPages}
+                className="pdf-next-page"
+                variant="contained"
+              >
+                Next Page
+              </Button>
+            </div>
+          )}
         </div>
       ) : null}
 
--- src/FrontEnd/src/components/Navbar/Navbar.tsx
+++ src/FrontEnd/src/components/Navbar/Navbar.tsx
@@ -1,44 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { List, ListItem, ListItemIcon } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import './Navbar.css';
 
-const userRole = localStorage.getItem('userRole');
-
 const Navbar: React.FC = () => {
-    const navigate = useNavigate();
+  const [userRole, setUserRole] = useState<string | null>(null);
+  const navigate = useNavigate();
 
-    const handleAccountClick = () => {
-        navigate('/account');
-    };
+  useEffect(() => {
+    const storedUserRole = localStorage.getItem('userRole');
+    setUserRole(storedUserRole);
+  }, []);
 
-    const handleHomeClick = () => {
-        navigate('/home');
-    }
+  const handleAccountClick = () => {
+    navigate('/account');
+  };
 
-    return (
-        <div className="navbar">
-            <div className="nav-container">
-                <List>
-                    <ListItem button onClick={handleHomeClick}>
-                        <ListItemIcon>
-                            <HomeIcon />
-                        </ListItemIcon>
-                    </ListItem>
-                    {userRole === 'Administrator' && (
-                        <ListItem button onClick={handleAccountClick}>
-                            <ListItemIcon>
-                                <AccountCircleIcon />
-                            </ListItemIcon>
-                        </ListItem>
-                    )}
-                </List>
-                <div className="navbar-line"></div>
-            </div>
-        </div>
-    );
+  const handleHomeClick = () => {
+    navigate('/home');
+  };
+
+  return (
+    <div className="navbar">
+      <div className="nav-container">
+        <List>
+          <ListItem button onClick={handleHomeClick}>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+          </ListItem>
+          {userRole === 'Administrator' && (
+            <ListItem button onClick={handleAccountClick}>
+              <ListItemIcon>
+                <AccountCircleIcon />
+              </ListItemIcon>
+            </ListItem>
+          )}
+        </List>
+        <div className="navbar-line"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
